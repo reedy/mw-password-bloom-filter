130cd8fed76ebc49871d7aac312e101d441aec94
Sam Reed <sam@reedyboy.net>
2017-09-12 18:06:50 +0100

    Create a bloomfilter using MakinaCorpus\Bloom\BloomFilter
    Commit serialised .ser file (for ease of testing)
    
    .gitignore vendor/composer stuffs

---
+++ createBloomFilter.php
@@ -0,0 +1,29 @@
+<?php
+
+require __DIR__ . '/vendor/autoload.php';
+
+use MakinaCorpus\Bloom\BloomFilter;
+
+$totalTime = -microtime( true );
+
+$filter = new BloomFilter( 100000, 0.001 );
+
+if ( $file = fopen("10_million_password_list_top_100000.txt", "r" ) ) {
+	while( !feof( $file ) ) {
+		$line = fgets( $file );
+		if ( !$line ) {
+			continue;
+		}
+		$filter->set( fgets( $file ) );
+	}
+	fclose( $file );
+}
+
+$totalTime += microtime( true );
+
+echo sprintf(
+	"\nBuilding bloom filter for 100000 passwords %.1f seconds\n",
+	$totalTime
+);
+
+file_put_contents( '10_million_password_list_top_100000.ser', $filter->serialize() );
