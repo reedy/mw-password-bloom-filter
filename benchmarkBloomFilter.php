f23da1855e3ca196356ce772706fb4f36d1ce501
Sam Reed <sam@reedyboy.net>
2017-09-12 22:48:53 +0100

    Add some nice benchmarking functions
    Lovingly stolen from:
    https://github.com/wikimedia/mediawiki/blob/cfb1379/maintenance/benchmarks/Benchmarker.php
    https://github.com/wikimedia/mediawiki/blob/3f1a528/languages/Language.php

--- benchmarkBloomFilter.php
+++ benchmarkBloomFilter.php
@@ -2,6 +2,14 @@
 
 require_once 'BloomFilterWrapper.php';
 
+echo sprintf( "Running PHP version %s (%s) on %s %s %s\n",
+	phpversion(),
+	php_uname( 'm' ),
+	php_uname( 's' ),
+	php_uname( 'r' ),
+	php_uname( 'v' )
+);
+
 $passwords = BloomFilterWrapper::getPasswords();
 foreach ( BloomFilterWrapper::getFilterNames() as $filterName ) {
 	$totalTime = -microtime( true );
@@ -17,9 +25,18 @@ foreach ( BloomFilterWrapper::getFilterNames() as $filterName ) {
 	echo sprintf( "\nBuilding $filterName bloom filter for 100000 passwords in %.1f seconds\n", $totalTime );
 
 	$totalTime = -microtime( true );
+
 	$failCount = 0;
+	shuffle( $passwords );
+
+	$times = [];
+
 	foreach( $passwords as $password ) {
-		if ( !$filter->get( $password ) ) {
+		$time = -microtime( true );
+		$res = $filter->get( $password );
+		$time += microtime( true );
+		$times[] = $time;
+		if ( !$res ) {
 			$failCount++;
 			echo "'$password' isn't found by $filterName bloom filter.\n";
 		}
@@ -30,4 +47,52 @@ foreach ( BloomFilterWrapper::getFilterNames() as $filterName ) {
 	$totalTime += microtime( true );
 
 	echo sprintf( "Testing $filterName bloom filter for 100000 passwords in %.1f seconds\n", $totalTime );
+
+	$count = count( $times );
+	sort( $times, SORT_NUMERIC );
+	$min = $times[0];
+	$max = end( $times );
+	if ( $count % 2 ) {
+		$median = $times[ ( $count - 1 ) / 2 ];
+	} else {
+		$median = ( $times[$count / 2] + $times[$count / 2 - 1] ) / 2;
+	}
+	$total = array_sum( $times );
+	$mean = $total / $count;
+
+	addResult( [
+		'name' => $filterName,
+		'count' => $count,
+		'total' => $total,
+		'min' => $min,
+		'median' => $median,
+		'mean' => $mean,
+		'max' => $max,
+		'usage' => [
+			'mem' => memory_get_usage( true ),
+			'mempeak' => memory_get_peak_usage( true ),
+		],
+	] );
+}
+
+function addResult( $res ) {
+	$ret = sprintf( "%s\n  %' 6s: %d\n", $res['name'], 'times', $res['count'] );
+	foreach ( [ 'total', 'min', 'median', 'mean', 'max' ] as $metric ) {
+		$ret .= sprintf( "  %' 6s: %6.2fms\n", $metric, $res[$metric] );
+	}
+	foreach ( [ 'mem' => 'Current memory usage', 'mempeak' => 'Peak memory usage' ] as $key => $label ) {
+		$ret .= sprintf( "%' 20s: %s\n", $label, formatComputingNumbers( $res['usage'][$key] ) );
+	}
+	echo "$ret\n";
+}
+
+function formatComputingNumbers( $size, $boundary = 1024 ) {
+	$sizes = [ '', 'kilo', 'mega', 'giga', 'tera', 'peta', 'exa', 'zeta', 'yotta' ];
+	$index = 0;
+	$maxIndex = count( $sizes ) - 1;
+	while ( $size >= $boundary && $index < $maxIndex ) {
+		$index++;
+		$size /= $boundary;
+	}
+	return "$size {$sizes[$index]}bytes";
 }
